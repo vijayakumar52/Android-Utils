4a9eb3a27f41d856eb7934eefa08b4ed2e45cecf
vijayakumar52 <vijayakumar4057@gmail.com>
2017-12-11 22:51:56 +0530

    Updating utils

--- src/main/java/com/vijay/androidutils/PrefUtils.java
+++ src/main/java/com/vijay/androidutils/PrefUtils.java
@@ -1,85 +1,71 @@
-package com.vijay.androidutils;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+package com.vijay.locationtracker;
 
 /**
- * Created by vijay-3593 on 09/12/17.
+ * Created by vijay-3593 on 19/11/17.
  */
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 public class PrefUtils {
+    private static final String SHARED_PREF = "PREFERENCE";
 
-    private Context mContext;
-
-    private SharedPreferences mSettings;
-    private Editor mEditor;
-
-    public PrefUtils(Context ctx, String prefFileName) {
-        mContext = ctx;
-
-        mSettings = mContext.getSharedPreferences(prefFileName,
-                Context.MODE_PRIVATE);
-        mEditor = mSettings.edit();
+    static public String getPrefValueString(Context activity, String key){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        return sharedPreference.getString(key, ""); //No i18N
     }
 
-    public void setValue(String key, String value) {
-        mEditor.putString(key, value);
-        mEditor.commit();
-    }
-
-    public void setValue(String key, int value) {
-        mEditor.putInt(key, value);
-        mEditor.commit();
-    }
-
-
-    public void setValue(String key, double value) {
-        setValue(key, Double.toString(value));
-    }
-
-
-    public void setValue(String key, long value) {
-        mEditor.putLong(key, value);
-        mEditor.commit();
-    }
-
-
-    public String getValue(String key, String defaultValue) {
-        return mSettings.getString(key, defaultValue);
-    }
-
-    public int getIntValue(String key, int defaultValue) {
-        return mSettings.getInt(key, defaultValue);
-    }
-
-    public long getLongValue(String key, long defaultValue) {
-        return mSettings.getLong(key, defaultValue);
-    }
-
-    public boolean getValue(String key, boolean defValue) {
-        return mSettings.getBoolean(key, defValue);
-    }
-
-    public void setValue(String key, boolean value) {
-        mEditor.putBoolean(key, value);
-        mEditor.commit();
-    }
-
-    public void removeValue(String key) {
-        if (mEditor != null) {
-            mEditor.remove(key).commit();
+    static public String getStrictPrefValueString(Activity activity, String key)throws Exception{
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        if(sharedPreference.contains(key)){
+            return sharedPreference.getString(key, ""); //No i18N
+        }
+        else{
+            throw new Exception();
         }
     }
 
-    public boolean clear() {
-        try {
-            mEditor.clear().commit();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+    static public boolean setPrefValueString(Context activity, String key, String value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putString(key, value);
+        return editor.commit();
+    }
+
+    static public boolean setPrefValueBoolean(Context activity,String key,boolean value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putBoolean(key, value);
+        return editor.commit();
+    }
+    static public boolean setPrefValueLong(Context activity,String key,long value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putLong(key, value);
+        return editor.commit();
+    }
+    static public boolean setPrefValueInt(Context activity,String key,int value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putInt(key, value);
+        return editor.commit();
+    }
+    static public boolean getPrefValueBoolean(Context activity, String key){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        return sharedPreference.getBoolean(key, false);
+    }
+    static public long getPrefValueLong(Context activity, String key){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        return sharedPreference.getLong(key,-1);
+    }
+    static public int getPrefValueInt(Context activity, String key){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        return sharedPreference.getInt(key,0);
+    }
+    static public void deletePrefValue(Context activity,String key){
+        SharedPreferences prefs = activity.getSharedPreferences(SHARED_PREF, 0);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.remove(key);
+        edit.commit();
     }
 }
