26d82d04e411b3505e0f7dd7bfab28b5f6cee507
Vijay <vijayakumar4057@gmail.com>
2017-12-24 00:23:58 +0530

    Updating PrefUtils

--- src/main/java/com/vijay/androidutils/PrefUtils.java
+++ src/main/java/com/vijay/androidutils/PrefUtils.java
@@ -10,6 +10,29 @@ import android.content.SharedPreferences;
 public class PrefUtils {
     private static final String SHARED_PREF = "PREFERENCE";
 
+
+    static public void setPrefValueString(Context activity, String key, String value){
+        SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putString(key, value).apply();
+    }
+
+    static public void setPrefValueBoolean(Context activity,String key,boolean value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putBoolean(key, value).apply();
+    }
+    static public void setPrefValueLong(Context activity, String key, long value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putLong(key, value).apply();
+    }
+    static public void setPrefValueInt(Context activity, String key, int value){
+        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
+        final SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putInt(key, value).apply();
+    }
+
     static public String getPrefValueString(Context activity, String key){
         final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
         return sharedPreference.getString(key, ""); //No i18N
@@ -25,31 +48,6 @@ public class PrefUtils {
         }
     }
 
-    static public boolean setPrefValueString(Context activity, String key, String value){
-        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
-        final SharedPreferences.Editor editor = sharedPreference.edit();
-        editor.putString(key, value);
-        return editor.commit();
-    }
-
-    static public boolean setPrefValueBoolean(Context activity,String key,boolean value){
-        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
-        final SharedPreferences.Editor editor = sharedPreference.edit();
-        editor.putBoolean(key, value);
-        return editor.commit();
-    }
-    static public boolean setPrefValueLong(Context activity,String key,long value){
-        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
-        final SharedPreferences.Editor editor = sharedPreference.edit();
-        editor.putLong(key, value);
-        return editor.commit();
-    }
-    static public boolean setPrefValueInt(Context activity,String key,int value){
-        final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
-        final SharedPreferences.Editor editor = sharedPreference.edit();
-        editor.putInt(key, value);
-        return editor.commit();
-    }
     static public boolean getPrefValueBoolean(Context activity, String key){
         final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
         return sharedPreference.getBoolean(key, false);
@@ -60,12 +58,11 @@ public class PrefUtils {
     }
     static public int getPrefValueInt(Context activity, String key){
         final SharedPreferences sharedPreference = activity.getSharedPreferences(SHARED_PREF, 0);
-        return sharedPreference.getInt(key,0);
+        return sharedPreference.getInt(key,-1);
     }
     static public void deletePrefValue(Context activity,String key){
         SharedPreferences prefs = activity.getSharedPreferences(SHARED_PREF, 0);
         SharedPreferences.Editor edit = prefs.edit();
-        edit.remove(key);
-        edit.commit();
+        edit.remove(key).apply();
     }
 }
