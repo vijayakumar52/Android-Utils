6885cc8a290ad622e0da7d569a1a19e4d7dae58d
Vijay <vijayakumar4057@gmail.com>
2018-01-07 23:04:30 +0530

    Date Utils updated

--- src/main/java/com/vijay/androidutils/DateUtils.java
+++ src/main/java/com/vijay/androidutils/DateUtils.java
@@ -1,5 +1,7 @@
 package com.vijay.androidutils;
 
+import android.text.format.DateFormat;
+
 import org.ocpsoft.prettytime.PrettyTime;
 
 import java.util.Date;
@@ -12,5 +14,14 @@ public class DateUtils {
     public static String getRelativeTime(Long time) {
         PrettyTime prettyTime = new PrettyTime();
         return prettyTime.format(new Date(time));
+    }
+
+    public static String getDate(Long dateInMilliseconds, String dateFormat) {
+        return DateFormat.format(dateFormat, dateInMilliseconds).toString();
+    }
+
+    public static String getDate(Long dateInMilliseconds) {
+        String format = "dd/MM/yyyy hh:mm:ss";
+        return DateFormat.format(format, dateInMilliseconds).toString();
     }
 }
